2e97804738f3b01090484cb9b1ae280d0b0c764c
Jonatan Gomez Perdomo <jgomezpe@unal.edu.co>
2022-05-18 12:11:31 -0500

    Gabo compact version

--- python/gabo.py
+++ python/gabo.py
@@ -34,57 +34,41 @@ def C(x, y, fx, fy, k):
   contribution[k].append(c)
   return c
 
-# Checks the gene's contribution information to determine if the best bit value (allele)
-# for the gene is 0: c<0, or 1: c>0. If c=0 then a gene behaves like an intron (neutral) so 
-# bit is set to a random value. Returns a non-negative contribution (c) accordingly
-def allele(c):
-  if(c>0): return 1,c
-  if(c<0): return 0,-c
-  bit = 0 if randbool() else 1
-  return bit, c
-
 # Checks all the information about the gene's contribution to get the higher one,
 # determines the best allele (0, 1, or None) for the gene  and the first time (in checking trials) it was reached
 def best_allele(k):
   global contribution
-  alle, cont = allele(contribution[k][0])
-  trial = 0
-  for i in range(1,len(contribution[k])):
-    a, c = allele(contribution[k][i])
-    if(c > cont): alle, cont, trial = a, c, i
-  return alle, cont, trial
+  ba, bc = -1,-1
+  for c in contribution[k]:
+    if(c>0): oa,oc = 1,c
+    elif(c<0): oa,oc =  0,-c
+    else: oa,oc = 0 if randbool() else 1, 0
+    if(oc > bc): ba, bc = oa, oc
+  return ba
 
 # Best by gene contribution
-def best_by_gene_contribution(f, evals, x, fx):
+def genome_best_gene_contribution(f, evals, x, fx):
   if(evals>0):
-    y = [best_allele(k)[0] for k in range(len(x))] 
+    y = [best_allele(k) for k in range(len(x))] 
     fy = evaluate(f,y)
     x, y, fx, fy = pick(x, y, fx, fy)
     evals -= 1
   return x, fx, evals
   
 # initializes global variables
-def init_GABO(f, evals, x, fx=None):
+def ICSplit(f, evals, x, fx):
   global intron, coding, separable, contribution
-  D = len(x)
-  separable = [True for k in range(D)]
-  contribution = [[] for k in range(D)]
-  intron = [k for k in range(D)]
-  coding = []
-  if(evals>0 and not fx):
-    fx = evaluate(f,x)
-    evals -= 1
-  P = permutation(D)
+  P = permutation(len(x))
   for k in P:
     if(evals==0): return x, fx, evals
     y = flip(x,k)
     fy = evaluate(f,y)
     evals -= 1    
     x, y, fx, fy = pick(x, y, fx, fy)
-    if( C(x, y, fx, fy, k) != 0 ): #Determines type of gene intron/coding
+    if( C(x, y, fx, fy, k) != 0 ): 
       intron.remove(k)
       coding.append(k)  
-  return best_by_gene_contribution(f, evals, x, fx)
+  return genome_best_gene_contribution(f, evals, x, fx)
 
 # Introns only search algorithm
 # f: Function to be optimized
@@ -94,18 +78,16 @@ def init_GABO(f, evals, x, fx=None):
 def IOSA( f, evals, x, fx):
   global intron
   N = len(intron)
-  if(N==0): return x, fx, evals
   for i in range(N):
     if(evals==0): return x, fx, evals
-    j = rand.randint(0,N-1)
-    k = intron[j] # Picks and analyzes one intron-like locus
+    a = rand.randint(0,N-1)
+    k = intron[a] # Picks and analyzes one intron-like locus
     y = flip(x,k)
     fy = evaluate(f,y)
     evals -= 1    
     x, y, fx, fy = pick(x, y, fx, fy)
-    # Checks if the locus is not intron-like and removes it from intron-like list
     if( C(x, y, fx, fy, k) != 0 ):
-      intron.pop(j)
+      intron.pop(a)
       N-=1
   return x, fx, evals
 
@@ -115,18 +97,17 @@ def IOSA( f, evals, x, fx):
 # x: initial point
 # fx: f value at point x (if provided)
 # evals: Maximum number of fitness evaluations
-def GCA( f, evals, x, fx ):
+def COSA( f, evals, x, fx ):
   global coding, separable
-  N = len(coding) # Space dimension
-  if(N==0): return x, fx, evals
-  if(evals>0):
-    # The complement candidate solution (used for determining locus separability)
-    xc = complement(x)
-    fxc = evaluate(f,xc)
-    evals -= 1
-    x, xc, fx, fxc = pick(x, xc, fx, fxc)
+  N = len(coding)
+  if(N==0 or evals==0): return x, fx, evals
 
-  # Considers locus by locus (shuffles loci for reducing order effect)
+  xc = complement(x)
+  fxc = evaluate(f,xc)
+  evals -= 1
+  x, xc, fx, fxc = pick(x, xc, fx, fxc)
+
+  # Considers locus by locus in a random fashion
   perm = permutation(N)
   for i in perm:
     k = coding[i]
@@ -143,15 +124,18 @@ def GCA( f, evals, x, fx ):
     y, yc, fy, fyc = pick( y, yc, fy, fyc )
     x, y, fx, fy = pick( x, y, fx, fy )
     if(w!=x): xc, fxc = yc, fyc
-  if(evals>0):
-    y = [best_allele(k)[0] for k in range(len(x))] 
-    fy = evaluate(f,y)
-    x, y, fx, fy = pick(x, y, fx, fy)
-    evals -= 1
+  x, fx, evals = genome_best_gene_contribution(f, evals, x, fx)
   return x, fx, evals
 
+def init(D):
+  global intron, coding, separable, contribution
+  separable = [True for k in range(D)]
+  contribution = [[] for k in range(D)]
+  intron = [k for k in range(D)]
+  coding = []
+
 def stop():
-  global coding, separable
+  global coding, intron, separable
   for k in coding:
     if(not separable[k]): return False
   return len(intron)==0
@@ -161,10 +145,14 @@ def stop():
 # evals: Maximum number of fitness evaluations
 # fx: f value at point x (if provided)
 def GABO( f, evals, x, fx=None):
-  x, fx, evals = init_GABO(f, evals, x, fx)
+  init(len(x))
+  if(evals>0 and not fx):
+    fx = evaluate(f,x)
+    evals -= 1
+  x, fx, evals = ICSplit(f, evals, x, fx)
   flag = True
   while(evals>0 and flag):
     x, fx, evals = IOSA(f, evals, x, fx)
-    x, fx, evals = GCA(f, evals, x, fx)
+    x, fx, evals = COSA(f, evals, x, fx)
     flag = not stop()
   return x, fx 
