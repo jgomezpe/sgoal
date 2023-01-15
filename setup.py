e9fd6883dcb1a1e9be82e5696f5144945e6d841a
Jonatan Gomez Perdomo <jgomezpe@unal.edu.co>
2023-01-14 23:54:02 -0500

    Fix package structure

--- setup.py
+++ setup.py
@@ -3,16 +3,16 @@
 from setuptools import setup
 
 setup(name='sgoal',
-                 version=Version('1.0.0').number,
-                 description='Stochastic Global Optimization Algorithms Python Package',
-                 long_description=open('README.md').read().strip(),
-                 author='Jonatan Gomez',
-                 author_email='jgomezpe@unal.edu.co',
-                 url='https://github.com/jgomezpe/sgoal',
-                 py_modules=['sgoal'],
-                 install_requires=[],
-                 license='MIT License',
-                 zip_safe=False,
-                 keywords='sgoal package',
-                 classifiers=['Packages', 'SGoal']
-      )
+      version=Version('1.0.0').number,
+      description='Stochastic Global Optimization Algorithms Python Package',
+      long_description=open('README.md').read().strip(),
+      author='Jonatan Gomez',
+      author_email='jgomezpe@unal.edu.co',
+      url='https://github.com/jgomezpe/sgoal',
+      py_modules=['sgoal'],
+      install_requires=[],
+      license='MIT License',
+      zip_safe=False,
+      keywords='sgoal package',
+      classifiers=['Packages', 'SGoal']
+)
