548b69a67b71067612d7a1e58b163b16a83f35f9
Jonatan Gomez Perdomo <jgomezpe@unal.edu.co>
2023-01-14 23:04:03 -0500

    Fix setup.py

--- python/sgoal/setup.py
+++ python/sgoal/setup.py
@@ -5,8 +5,6 @@ from setuptools import setup
 setup(name='sgoal',
       version='1.0',
       # list folders, not files
-      packages=['sgoal',
-                'capitalize.test'],
-      scripts=['capitalize/bin/cap_script.py'],
-      package_data={'capitalize': ['data/cap_data.txt']},
+      packages=['sgoal'],
+      scripts=['sgoal/bin/sgoal_script.py'],
       )
